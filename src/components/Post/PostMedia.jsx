748bc5bc13f9c0fdfcb5438d9c89cd652f838b02
orelk <orelkanditan@gmail.com>
2019-07-05 17:12:34 +0300

    Changed to non-animation expand because of FPS issues

--- src/components/Post/PostMedia.jsx
+++ src/components/Post/PostMedia.jsx
@@ -1,29 +1,26 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
 import PropTypes from "prop-types";
-import { Typography, ExpansionPanel, ExpansionPanelDetails } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import Markdown from 'react-markdown';
 import he from 'he';
 import Video from './Video';
 import AnimatedHover from './AnimatedHover';
 
-const sourceImg = css`
-  max-height: 30em;
-  max-width: 100%;
-  margin: auto 0;
-  display: block;
-`;
-const collapse = css`
+const content = css`
   display: flex;
   justify-content: center;
-  padding: 0;
-`;
-const collapseContent = css`
   padding: 0 2em 1em 2em;  
   @media only screen and (max-width: 768px) {
     padding: 5%;
     padding-top: 0;
   }
+`;
+const sourceImg = css`
+  max-height: 30em;
+  max-width: 100%;
+  margin: auto 0;
+  display: block;
 `;
 const text = css`
   overflow: auto;
@@ -40,24 +37,20 @@ const text = css`
 function PostMedia({ post, expanded, hasImage, 
   hasVideo, hasEmbed, hasText, htmlEmbed }) {
   
-  return (
-    <ExpansionPanel expanded={expanded}>
-      <ExpansionPanelDetails css={collapse}>
-        <div css={collapseContent}>
-          {hasImage && 
-            <AnimatedHover>
-              <img src={post.url} alt='source' css={sourceImg}/>
-            </AnimatedHover>}
-          {hasVideo && <Video src={post.media.reddit_video.fallback_url}/>}
-          {hasEmbed && 
-            <div dangerouslySetInnerHTML={{__html: htmlEmbed}}/>}
-          {hasText && 
-            <Typography variant='h5' css={text}>
-              <Markdown source={he.decode(post.selftext)}/>
-            </Typography>}
-        </div>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
+  return (expanded &&
+    <div css={content}>
+      {hasImage && 
+        <AnimatedHover>
+          <img src={post.url} alt='source' css={sourceImg}/>
+        </AnimatedHover>}
+      {hasVideo && <Video src={post.media.reddit_video.fallback_url}/>}
+      {hasEmbed && 
+        <div dangerouslySetInnerHTML={{__html: htmlEmbed}}/>}
+      {hasText && 
+        <Typography variant='h5' css={text}>
+          <Markdown source={he.decode(post.selftext)}/>
+        </Typography>}
+    </div>
   );
 }
 
